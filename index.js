396454769241c17cf1aa24280dcb3b3b2f338aad
chan0617 <mo0om487@naver.com>
2022-10-05 10:20:12 +0900

    서버 라우팅적용

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
-var http=require("http");
-var hostname='127.0.0.1';
-var port='8080';
+const express=require("express");
+const cors=require("cors");
+const app=express();
+const port=8080;
 
-const server=http.createServer(function(req,res){
-    // 한글사용 노노
-    //console.log('request:',req);
-    const path=req.url;
-    // 요청들어온 경로를 path에 저장
-    const method=req.method;
-    if(path==='/products'){
-        if(method==='GET'){
-            res.writeHead(200,{"Content-Type":"application/json"});
-            const products =JSON.stringify([{name:"배변패드",price:50000}]);
-            //stringify 자바스크립트 형식을 제이슨방식으로 변환시켜준다.
-            res.end(products);
-        }else if(method==='POST'){
-            res.end("POST success");
-        }
-        res.end('GOOD BOY');
-    }
-    
-    res.end('user Hello');
+app.use(express.json());
+app.use(cors());
+app.get('/products',(req,res)=>{
+    const queryString=req.query;
+    console.log(queryString.id)
+    res.send(
+        {
+            "products": [
+            {"id":1, "name": "습식사료", "price": 10000, "seller": "내추럴코어", "imageUrl": "images/products/food1.jpg"},
+            {"id":2, "name": "하네스", "price": 50000, "seller": "도기멍", "imageUrl": "images/products/acc1.jpg"},
+            {"id":3, "name": "배변패드", "price": 30000, "seller": "흡수혁명", "imageUrl": "images/products/house1.jpg"}
+            ]
+        }            
+    );
+});
+app.get("/products/:id/events/:eventId",(req,res)=>{
+    const params = req.params;
+    const {id,eventId} = params;
+    res.send(`id는 ${id} 와 ${eventId}입니다`)
+  })
+app.post('/products',(req,res)=>{
+    const body=req.body;
+    res.send(
+    {body:body}
+    );
+});
+app.get('/login',(req,res)=>{
+    res.send('로그인해주세요')
+});
+app.listen(port,()=>{
+    console.log('망고샵의 서버가 돌아가고 있습니다.')
 })
 
-server.listen(port,hostname);
-console.log('react-projects-server on');
+
+
 
 
 
